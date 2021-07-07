e87f57ba010b53dcdeb12325d252d7129c247105
christophermarek <christophermarek42@gmail.com>
2021-07-07 12:04:03 -0400

    added realtime code refresh on update

--- src/app.ts
+++ src/app.ts
@@ -6,5 +6,5 @@ const port = 3000;
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
