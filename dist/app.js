9f07425e82de37be7aa754d506c4f74ce33f2c48
christophermarek <christophermarek42@gmail.com>
2021-07-07 15:33:47 -0400

    poor implementation right now

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-const port = 3000;
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-    console.log(`[server]: Server is running at https://localhost:${PORT}`);
-});
+const blockchain_1 = require("./blockchain");
+const p2p_1 = require("./p2p");
+//probably replace with env variables after
+const httpPort = 3001;
+const p2pPort = 6001;
+const initHttpServer = (myHttpPort) => {
+    const app = express_1.default();
+    app.use(body_parser_1.default.json());
+    app.get('/blocks', (req, res) => {
+        res.send(blockchain_1.getBlockchain());
+    });
+    app.post('/mineBlock', (req, res) => {
+        const newBlock = blockchain_1.generateNextBlock(req.body.data);
+        res.send(newBlock);
+    });
+    app.get('/peers', (req, res) => {
+        res.send(p2p_1.getSockets().map((s) => s._socket.remoteAddress + ':' + s._socket.remotePort));
+    });
+    app.post('/addPeer', (req, res) => {
+        p2p_1.connectToPeers(req.body.peer);
+        res.send();
+    });
+    app.listen(myHttpPort, () => {
+        console.log('Listening http on port: ' + myHttpPort);
+    });
+};
+initHttpServer(httpPort);
+p2p_1.initP2PServer(p2pPort);
