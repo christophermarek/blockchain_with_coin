cd08fcaa9f179227e4bc499c5843187425ecc02d
christophermarek <christophermarek42@gmail.com>
2021-07-07 21:11:17 -0400

    explorer done until wallet created

--- blockchain_explorer/src/App.tsx
+++ blockchain_explorer/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import axios from 'axios';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface block {
   data: [
@@ -35,22 +35,29 @@ function App() {
 
   const [blockChain, setBlockChain] = useState<Array<block>>([]);
 
-  async function getBlockChain() {
-    try {
-      const response = await axios.get('http://localhost:3001/blocks');
-      let blocks = response.data;
-      setBlockChain(blocks);
-    } catch (error) {
-      console.error(error);
-      setBlockChain([]);
+  useEffect(() => {
 
+    async function getBlockChain() {
+      try {
+        const response = await axios.get('http://localhost:3001/blocks');
+        let blocks = response.data;
+        setBlockChain(blocks);
+      } catch (error) {
+        console.error(error);
+        setBlockChain([]);
+  
+      }
     }
-  }
+
+    getBlockChain();
+
+  });
+
+  
 
   return (
     <div className="App">
       <p>BlockChain Explorer</p>
-      <input type='button' value='Get BlockChain' onClick={() => getBlockChain()} />
 
       {blockChain.length < 1 ?
         (
@@ -61,17 +68,36 @@ function App() {
           blockChain.map((block: block, index: number) => (
             <div key={index}>
               <p>Block #{index}</p>
+              {index === 0 &&
+                <p>Genesis Block</p>
+              }
               <div className='container'>
-                <div className='titles'>
-                  <p>Hash</p>
-                  <p>Previous Hash</p>
-                  <p>Timestamp</p>
-                  <p>Difficulty</p>
-                  <p>Nonce</p>
-                  <p>Number of transactions</p>
-                </div>
                 <div className='values'>
-
+                  <div className='row'>
+                    <p className='rowItem'>Hash</p>
+                    <p className='rowItem'>{block.hash}</p>
+                  </div>
+                  <div className='row'>
+                    <p className='rowItem'>Previous Hash</p>
+                    <p className='rowItem'>{block.previousHash}</p>
+                  </div>
+                  <div className='row'>
+                    <p className='rowItem'>Timestamp</p>
+                    <p className='rowItem'>{block.timestamp}</p>
+                  </div>
+                  <div className='row'>
+                    <p className='rowItem'>Difficulty</p>
+                    <p className='rowItem'>{block.difficulty}</p>
+                  </div>
+                  <div className='row'>
+                    <p className='rowItem'>Nonce</p>
+                    <p className='rowItem'>{block.nonce}</p>
+                  </div>
+                  <div className='row'>
+                    <p className='rowItem'>Number of transactions</p>
+                    {/* Gotta change this once i test more transactions bc no way the num transactions is index 0 only */}
+                    <p className='rowItem'>{block.data[0].txIns.length + block.data[0].txOuts.length}</p>
+                  </div>
                 </div>
               </div>
             </div>
